762feaf274abab3ad1922474404c33d170a680fc
xtrasee <thn018@ucsd.edu>
2024-02-06 00:07:02 -0800

    javascript variables and working on addIteem function

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ const groceryList = document.getElementById('grocery-list');
 const foodItem = document.getElementsByClassName('title');
 
 // buttons
-const submitBtn = document.querySelector('submit-btn');
-const editBtn = document.querySelector('edit-btn');
-const deleteBtn = document.querySelector('delete-btn');
-const clearBtn = document.querySelector('clear-btn');
-const done = document.querySelector('finish-btn');
+const submitBtn = document.querySelector('.submit-btn');
+const editBtn = document.querySelector('.edit-btn');
+const deleteBtn = document.querySelector('.delete-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const done = document.querySelector('.finish-btn');
 
 // edit option
-
-// add function
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // event listeners
-submitBtn.addEventListener("click", addItem());
+submitBtn.addEventListener("click", addItem);
 
 
 // functions
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if (value !== '' && editFlag === false) {
+        console.log('add');
+    } else if (value !== '' && editFlag === true ) {
+        console.log('editing');
+    } else {
+        console.log('empty');
+    }
+}
 
 // local stoage
 
