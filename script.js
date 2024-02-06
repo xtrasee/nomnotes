fdc2bcb7c42c30f2e5e885f2619daae9d13c0a4a
xtrasee <thn018@ucsd.edu>
2024-02-05 23:47:41 -0800

    completed front end design

---
+++ script.js
@@ -0,0 +1,25 @@
+// select items
+const grocery = document.getElementById('item-input');
+const groceryList = document.getElementById('grocery-list');
+const foodItem = document.getElementsByClassName('title');
+
+// buttons
+const submitBtn = document.querySelector('submit-btn');
+const editBtn = document.querySelector('edit-btn');
+const deleteBtn = document.querySelector('delete-btn');
+const clearBtn = document.querySelector('clear-btn');
+const done = document.querySelector('finish-btn');
+
+// edit option
+
+// add function
+
+// event listeners
+submitBtn.addEventListener("click", addItem());
+
+
+// functions
+
+// local stoage
+
+// setup items
